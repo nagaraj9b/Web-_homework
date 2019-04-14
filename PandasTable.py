5334d3e1387cc754095fa503a3955315f1123765
Nagaraj <46365978+nagaraj9b@users.noreply.github.com>
2019-04-14 17:03:05 -0400

    Add files via upload

--- PandasTable.py
+++ PandasTable.py
@@ -3,11 +3,12 @@ import pandas as pd
 from numpy.random import randn
 from IPython.display import HTML
 
+cities_df = pd.read_csv('cities.csv')
 
-HTML(city_list_df.to_html(buf=None, columns=None, col_space=None, header=True, index=True, na_rep='NaN',
+HTML(cities_df.to_html(buf=None, columns=None, col_space=None, header=True, index=True, na_rep='NaN',
        formatters=None, float_format=None, sparsify=None, index_names=True, justify=None,
        bold_rows=True, classes='table table-striped table-bordered table-hover table-condensed', escape=True, max_rows=None, max_cols=None, show_dimensions=False,
        notebook=False, decimal='.', border=None))
 
 
-city_list_df.to_html("Table.html")
+cities_df.to_html("data.html")
